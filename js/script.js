c72e91da1324e0eb9d92cb4222b32dc5b936f2fd
LokeshBChinniah <57022188+LokeshBChinniah@users.noreply.github.com>
2020-03-18 12:58:22 -0400

    hideStudents
    Hide all students except for the ones displaying

--- js/script.js
+++ js/script.js
@@ -1,49 +1,19 @@
-/******************************************
-Treehouse Techdegree:
-FSJS project 2 - List Filter and Pagination
-******************************************/
-   
-// Study guide for this project - https://drive.google.com/file/d/1OD1diUsTMdpfMDv677TfL1xO2CEkykSz/view?usp=sharing
-
-
-/*** 
-   Add your global variables that store the DOM elements you will 
-   need to reference and/or manipulate. 
-   
-   But be mindful of which variables should be global and which 
-   should be locally scoped to one of the two main functions you're 
-   going to create. A good general rule of thumb is if the variable 
-   will only be used inside of a function, then it can be locally 
-   scoped to that function.
-***/
-const StudentList = document.getElementsByClassName('student-item');
+//Global Variables
+const studentsInList = document.getElementsByClassName('student-item');
 const studentsPerPage = 10;
 
+//Hide all students except for ones corresponding to the ones on the page number
+const hideStudents = (studentsInList, page) => {
+   var startIndex = (page * studentsPerPage) - studentsPerPage;
+   var endIndex = page * studentsPerPage;
 
-/*** 
-   Create the `showPage` function to hide all of the items in the 
-   list except for the ten you want to show.
-
-   Pro Tips: 
-     - Keep in mind that with a list of 54 students, the last page 
-       will only display four.
-     - Remember that the first student has an index of 0.
-     - Remember that a function `parameter` goes in the parens when 
-       you initially define the function, and it acts as a variable 
-       or a placeholder to represent the actual function `argument` 
-       that will be passed into the parens later when you call or 
-       "invoke" the function 
-***/
-
-function hideStudents(list, page) {
-   const startIndex = (page * numberOfStudents) - numberOfStudents;
-   const endIndex = page * numberOfStudents;
-
-   for (let i = startIndex; i < ) {
-
-      
+   for (let i = 0; i < startIndex.length; i += 1) {
+      if (i >= startIndex && i < endIndex ) {
+         studentsInList[i].style.display = '';
+      } else {
+         studentsInList[i].style.display = none;
+      }
    }
-
 }
 
 
@@ -51,7 +21,10 @@ function hideStudents(list, page) {
    Create the `appendPageLinks function` to generate, append, and add 
    functionality to the pagination buttons.
 ***/
+const appendPageLinks = (studentsInList) => {
+   const numOfPages = Math.ceil(studentsInList.length / studentsPerPage);
 
+}
 
 
 
